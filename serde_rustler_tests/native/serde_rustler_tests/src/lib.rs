c4ef718f9543e1878f18f39c78b9e2152aeaec46
Artur Panach <1930175+arturictus@users.noreply.github.com>
2022-09-15 12:36:08 +0100

    Save

--- serde_rustler_tests/native/serde_rustler_tests/src/lib.rs
+++ serde_rustler_tests/native/serde_rustler_tests/src/lib.rs
@@ -40,7 +40,7 @@ pub fn readme<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
 }
 
 /// Deserializes anything from an Elixir term and subsequently serializes the result back into an Elixir term, returning it.
-#[nif]
+#[rustler::nif]
 pub fn transcode<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     tag_tuple(env, || {
         let de = Deserializer::from(args[0]);
